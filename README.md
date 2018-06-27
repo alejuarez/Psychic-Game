547b0e0120e13b828d420ce9f5fdb3efea29c3cc
ale <ale-juarez@hotmail.ca>
2018-06-27 17:01:15 -0400

    psychic game working :)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Psychic-Game
+
+# The objective of the game is to guess a letter from the alphabet
+
+# You have up to 9 times to guess the letter the computer has chosen
+
+# If you type the same letter as the computer then you have won the game and it will reset the counter of number of guesses to 9 and add 1 to the wins score
+
+# if you didn't guess the letter the computer has chosen during the 9 times then it will add 1 to the loses score and sent you a a lost message
+
+# You can play the game as many times you want
+
+# Good luck!
---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+// Declare Variables
+var letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var userGuessesArray = [];
+var computerChoice = letters[Math.floor(Math.random() * (letters.length - 1))];
+console.log(computerChoice);
+
+// UI Functions
+// ==============================================================================
+
+function showUserGuesses() {
+  document.getElementById("userGuess").innerHTML = userGuessesArray;
+  console.log(userGuessesArray);
+}
+
+function showNumGuesses() {
+  document.getElementById("numOfGuess").innerHTML = numGuesses;
+}
+
+function showLosses() {
+  document.getElementById("losses").innerHTML = losses;
+}
+
+function showWins() {
+  document.getElementById("wins").innerHTML = wins;
+}
+// RESET - Sets inital state of the game
+
+function reset() {
+  computerChoice = letters[Math.floor(Math.random() * (letters.length - 1))];
+  userGuessesArray = [];
+  numGuesses = 9;
+  showUserGuesses();
+  showNumGuesses();
+  showLosses();
+  showWins();
+  console.log(computerChoice);
+  console.log("reset");
+}
+
+// before the game starts, call reset to initalize the game
+reset();
+
+// validates that the letter entered is in the array ..
+document.onkeyup = function(event) {
+  userGuess = event.key.toLowerCase();
+
+  if (!letters.includes(userGuess)) {
+    alert(
+      "Your choice is not a letter, please pick a letter from the alphabet"
+    );
+  } else if (userGuessesArray.includes(userGuess)) {
+    alert("You can only pick the same letter once");
+  } else {
+    userGuessesArray.push(userGuess);
+    numGuesses--;
+    showUserGuesses();
+    showNumGuesses();
+  }
+
+  if (userGuess === computerChoice) {
+    wins++;
+    //userGuessesArray.push(userGuess);
+    console.log("userguess igual" + userGuess);
+    console.log("userguess igual" + userGuessesArray);
+    showUserGuesses();
+    showWins();
+    alert("You Win, YAY!!!!");
+    reset();
+  }
+
+  if (numGuesses === 0) {
+    losses++;
+    showLosses();
+    alert("Sorry, your guess is not correct, try again!");
+    reset();
+  }
+};
